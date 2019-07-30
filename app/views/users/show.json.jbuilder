126205f1f03287cab8328f4f3f5643573e7850e1
Derek Wolpert <dfnj123@gmail.com>
2019-07-30 18:09:04 -0400

    Create root to best entry file

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :display_name, :biography, :country, :city, :profile_pic_url, :cover_pic_url
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  root to: 'static_pages#root'
+
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :edit, :show, :update, :destroy] 
-    end
+    resource :session, only: [:create, :destroy]
+  end
 end
