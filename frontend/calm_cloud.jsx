2300b97d84d651c7847a019b91453768eddd65c3
Derek Wolpert <dfnj123@gmail.com>
2019-07-31 11:59:51 -0400

    Created root.jsx and App.jsx files to facilitate easier implimentation of components

--- frontend/calm_cloud.jsx
+++ frontend/calm_cloud.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './util/session_api_util';
 import configureStore from './store/store'
+import Root from './components/root';
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const store = configureStore()
+    const store = configureStore();
 
     window.login = login;
     window.signup = signup;
@@ -15,5 +16,5 @@ document.addEventListener('DOMContentLoaded', () => {
     window.dispatch = store.dispatch;
 
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>testing... 1, 2, 3</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
