b2772bb535c66434cc7d80691dd26261a3c2788f
Derek Wolpert <dfnj123@gmail.com>
2019-07-31 18:32:46 -0400

    Correct file  naming inconsistencies

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
-import Modal from './modal/modal'
+import Modal from './modal/modal';
 import GreetingContainer from './greeting/greeting_container';
 
 const App = () => (
